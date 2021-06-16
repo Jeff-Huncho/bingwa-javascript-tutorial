b0a6cd9e8f4473a833dd7f0db65b15fd1c78541c
Jeff-Huncho <jeffhuncho0010@gmal.com>
2021-06-16 12:06:46 +0300

    Still trying although not succesful

--- index.js
+++ index.js
@@ -1,141 +1,22 @@
-
-/** 		var students = new Array("John", "Ann", "Aaron", "Edwin", "Elizabeth");
-		document.write("<b>Using for loops </b><br />");
-		for (i=0;i<students.length;i++)
-		{
-		document.write(students[i] + "<br />");
-		}
-
-
-Document.write ("<b>using do...while loops</b></br>")
-    do{
-	document.write(i+"<br />")
-	    i=i+2;
-      }while (i<20)
-
-var age = prompt("Please enter your age");
-	if(age>=18)
-		document.write("You are an adult <br />");
-		if(age<18)
-		document.write("You are NOT an adult <br />");
-
-var hours = new DAte() .getHours()
-    if (hours<12) {
-		document.write("Good Morning!!! <br/>");
-	} else 
-	    document.write("Good afternoon!!! <br />") 
-		
-	var one = prompt ("ENter the first number");
-    var two = prompt ("Enter the second number");
-	 one = parseInt (one);
-	 two = parseInt (two);
-	 if (one==two)
-	 document.write (one + "is equal to" + two + ".")
-	  else if (one<two) 
-	  document.write (one + "is less than" + two + ".")
-		else 
-		document.write (one +"is greater than" + two + ".") 
-	 */
-
-	/** 	function myFunction()
-		{
-			document.write("This is a simple function.<br />");
-		}
-		  myFunction();
-
-		  var count = 0;
-		function countConsonants(name)
-		{
-			for (var i=0;i<name.length;i++)
-			{
-              if(name[i] == "b" || name[i] == "c" || name[i] == "d" || name[i] == "k" || name[i] == "l" || name[i] == "m" || name[i] == "n" || name[i] == "p" || name[i] == "q" || name[i] == "s" || name[i] == "t" || name[i] == "v" || name[i] == "x" || name[i] == "y" || name[i] == "z" || name[i] == "h" || name[i] == "r" || name[i] == "w" || name[i] == "y")
-              count = count + 1;
-			}
-		document.write("Hello " + name + "!!! Your name has " + count + " Consonants.");
-		}
-   	 	var myName = prompt("Please enter your name");*/
-    	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		/**function myFunction(){
-
-			return confirm("do you like functions?")
-			//return 0;
-
-		
-		}
-
-		var answer = myFunction();
-
-		if (answer) {
-			document.write("Good Job")
-		} else {
-			document.write("Try other learning methods")
-		}
-		*/
-
-		/**function returnSum(first, second)
-        {
-          var sum = first + second;
-          return sum;
-        }
-      var firstNo  = 7803589433203;
-      var secondNo = 2232894987389;
-      document.write(firstNwo + " + " + secondNo + " = " + returnSum(secondNo,secondNo));*/
-
-	  document.cookie = "passWordCookie = cookievalue"	 
-
-	  localStorage.setItem('user', "passWord Cookie = cookievalue; expires= Wed, 16 Aug 2020 11:30:22 EAT"	 
-	  )
-
-		
-
-	
+function setCookie(username,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = username + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+setCookie("user_email","bobthegreat@gmail.com",30); //set "user_email" cookie, expires in 30 days
+var userEmail=getCookie("user_email");//"bobthegreat@gmail.com"
