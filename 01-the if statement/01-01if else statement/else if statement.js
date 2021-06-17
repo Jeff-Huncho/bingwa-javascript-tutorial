3f79fd9ef72cc19e422e5d3b0f68338fb4d7684b
Jeff-Huncho <jeffhuncho0010@gmal.com>
2021-06-17 09:35:30 +0300

    The else if statement.

---
+++ 01-the if statement/01-01if else statement/else if statement.js
@@ -0,0 +1,12 @@
+var course = part3;
+
+    if (course == 1) {
+    alert ("You have a long way to go keep pushing.")
+}
+
+    else if (course == 2) {
+        alert("Your getting there...")
+}
+    else (course ==3); {
+        alert("Keep up the good work")
+}
--- 01-the if statement/01-01if else statement/the else statement.js
+++ 01-the if statement/01-01if else statement/the else statement.js
@@ -16,3 +16,4 @@ else{
     alert ("ion know")
 }
 
+
