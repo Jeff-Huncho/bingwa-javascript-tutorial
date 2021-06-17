ae9096613bdfa6df8daa125c02636a6b4306a798
Jeff-Huncho <jeffhuncho0010@gmal.com>
2021-06-17 09:08:43 +0300

    The else statement.

---
+++ 01-the if statement/01-01if else statement/the else statement.js
@@ -0,0 +1,18 @@
+/** if (condition) {
+    
+}
+else {
+
+} **/
+
+var myAge = 15
+var herAge = 17
+
+if (herAge < myAge)
+
+alert("It is")
+
+else{
+    alert ("ion know")
+}
+
--- 01-the if statement/if else statement.js
+++ 01-the if statement/if else statement.js
@@ -6,6 +6,6 @@ if (condition){
 var myNum1 = 1988
 var myNum2 = 2021
 
-if (myNum1 > myNum2){
+if (myNum1 < myNum2){
     alert("Its a new era")
 }
