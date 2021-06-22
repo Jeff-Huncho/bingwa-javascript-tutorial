6a94c7dff697e129f33ea2c26b0c68aa33b29bc9
Jeff-Huncho <jeffhuncho0010@gmal.com>
2021-06-22 14:55:58 +0300

    Bingwa website

--- 03-01-functions/functions.js
+++ 03-01-functions/functions.js
@@ -5,10 +5,26 @@ function name(params) {
 }
     
 
-*/
 
-function myFunction () {
-    alert("This is a function!!")
+var Name = prompt ("Whats your name")
+function sayHello (Name) {
+   // alert("Hello" + Name)
 }
+document.write("Hello" +" " + Name)
 myFunction();
 
+
+function addNumbers("a + b") {
+    var c = (a+b);
+    return c;
+}
+document.write(addNumbers(a+b))
+*/
+
+function main() {
+    var amount = 110;
+    var rate = 1.1;
+    return (amount * rate)
+    }
+
+console.log(amount* rate);
